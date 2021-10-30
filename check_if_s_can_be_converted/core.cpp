208e15e1294b74dd06bb68895b95b54e0ee6d790
daviricci <davi.ricci@unesp.br>
2021-10-30 17:10:15 -0300

    Renomeia para ConcatRemove
    O método check_if_s_can_be_converted_to_t_in_k_operations foi covnertido
    para ConcatRemove para atender as especificações do exercício.

--- check_if_s_can_be_converted/core.cpp
+++ check_if_s_can_be_converted/core.cpp
@@ -14,12 +14,12 @@ namespace {
         return resp;
     }
 
-    bool check_if_s_can_be_converted_to_t_in_k_operations(const std::string& s, const std::string& t, int k){
-        if ((k>100)||(k<1)){std::cout << "no" << std::endl;return false;}
-        int l_min = std::min(s.length(), t.length());if(l_min<1){std::cout<<"no"<<std::endl;return false;}
-        int l_max = std::max(s.length(), t.length());if(l_max>100){std::cout<<"no"<<std::endl;return false;}
-        if (!check_if_chars_in_az_ascii(s)){std::cout<<"no"<<std::endl;return false;}
-        if (!check_if_chars_in_az_ascii(t)){std::cout<<"no"<<std::endl;return false;}
+    std::string ConcatRemove(const std::string& s, const std::string& t, int k){
+        if ((k>100)||(k<1)){return "no";}
+        int l_min = std::min(s.length(), t.length());if(l_min<1){return "no";}
+        int l_max = std::max(s.length(), t.length());if(l_max>100){return "no";}
+        if (!check_if_chars_in_az_ascii(s)){return "no";}
+        if (!check_if_chars_in_az_ascii(t)){return "no";}
         const char *p_s = s.c_str();
         const char *p_t = t.c_str();
         int l_similarity_now = 0;
@@ -32,12 +32,10 @@ namespace {
         int n_min_concat = t.length() - l_similarity_now;
         int n_min_total_mov = n_min_remove + n_min_concat;
         if (n_min_total_mov<=k){
-            std::cout << "yes" << std::endl;
-            return true;
+            return "yes";
         }
         else{
-            std::cout << "no" << std::endl;
-            return false;
+            return "no";
         }
     }
 }
--- check_if_s_can_be_converted/main.cpp
+++ check_if_s_can_be_converted/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     std::cin>>s;
     std::cin>>t;
     std::cin>>k;
-    check_if_s_can_be_converted_to_t_in_k_operations(s,t,k);
+    std::cout<<ConcatRemove(s,t,k)<<std::endl;
     system("pause");
     return 0;
 }
