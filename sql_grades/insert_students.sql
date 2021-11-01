187907ebd62b6c5d9a94e834a243ba4d42bfda37
daviricci <davi.ricci@unesp.br>
2021-11-01 11:08:55 -0300

    Implementa Ajuda para Eva gerar relatorio
    O problema descrito a seguir:
    '''
    Two tables are provided: Students and Notes.
    
    Students contains three columns: ID, Name and Value.
    
    Coluna  | Tipo
    ID          | Integer
    Name    | String
    Value     | Integer
    
    The table Notes contains three Integers columns: Grade, Min_Value and Max_Value filled according example bellow:
    
    Grade | Min_Value | Max_Value
    1          | 0                 |  9
    2          | 10               | 19
    3          | 20               | 29
    4          | 30               | 39
    5          | 40               | 49
    6          | 50               | 59
    7          | 60               | 69
    8          | 70               | 79
    9          | 80               | 89
    10        | 90               | 100
    
    Joana gives Eva the task of generating a report containing three columns: Name, Grade and Value. Joana does not want the names of students who received a grade lower than 8. The report must be in descending order by grade, that is, the highest grades are inserted first. If there are more than one student with the same grade (8-10) assigned to them, sort those students in particular by their names in alphabetical order. Finally, if the grade is less than 8, use "NULL" as the name and list them in grades in descending order. If there are more than one student with the same grade (1-7) assigned to them, sort those students in particular by their grades in ascending order.
    
    Write an SQL query to help Eva.
    
    Input example
    ID | Name           | Value
    1   | Julia             | 81
    2   | Carol            | 68
    3   | Maria           | 99
    4   | Andreia       | 78
    5   | Jaqueline     | 63
    6   | Marcela       | 88
    
    Output example
    Maria 10 99
    Marcela 9 88
    Julia 9 81
    Andreia 8 78
    NULL 7 63
    NULL 7 68
    Note: Print "NULL" in the name if the grade is less than 8.
    
    Explanation
    Consider the following table with the grades assigned to students:
    
    ID  | Name       | Value  | Grade
    1    | Julia         | 81       |   9
    2    | Carol        | 68       |   7
    3    | Maria       | 99       |   10
    4    | Andreia    | 78       |   8
    5    | Jaqueline  | 63       |   7
    6    | Marcela    | 88       |   9
    
    Thus, the following students obtained grades 8, 9 or 10:
    Mary (10)
    Marcela (9)
    Julia (9)
    Andreia (8)
    '''
    
    Foi implementado e resolvido. A princípio, esse foi implementado no
    Access. Mas, devido a limitação da ferramenta, esse foi re-implementado
    no MySql.
    A solução do problema se resume aos seguintes arquivos:
    insert_notes.sql -> para inserir as notas na tabela notes
    insert_students.sql -> para inserir os estudantes na tabela estudantes
    help_evaLmysql.sql -> consulta sql que retorna o relatório desejado.
    
    Os demais arquivos referem-se a base criada para simulação do problema

---
+++ sql_grades/insert_students.sql
@@ -0,0 +1,12 @@
+INSERT INTO students (ID,Name, Value)
+VALUES (1,'Julia', 81);
+INSERT INTO students (ID,Name, Value)
+VALUES (2,'Carol', 68);
+INSERT INTO students (ID,Name, Value)
+VALUES (3,'Maria', 99);
+INSERT INTO students (ID,Name, Value)
+VALUES (4,'Andreia', 78);
+INSERT INTO students (ID,Name, Value)
+VALUES (5,'Jaqueline', 63);
+INSERT INTO students (ID,Name, Value)
+VALUES (6,'Marcela', 88);
