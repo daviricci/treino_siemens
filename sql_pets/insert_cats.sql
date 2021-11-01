a491f8bd521e20fb0d4c5316770029a845cee7fd
daviricci <davi.ricci@unesp.br>
2021-11-01 11:41:47 -0300

    Busca nomes únicos dos pets
    O problema
    '''
    7.Information about pets is kept in two separate tables:
    
    TABLE dogs
    id INTEGER NOT NULL PRIMARY KEY,
    name VARCHAR(50) NOT NULL
    
    TABLE cats
    id INTEGER NOT NULL PRIMARY KEY,
    name VARCHAR(50) NOT NULL
    
    Write an SQL query that select all distinct pet names.
    '''
    Foi resolvido e implementado.
    A solução se resume aos arquivos:
    insert_cats.sql -> para inserir os registros dos pets
    insert_digs.sql -> para inserir os registros dos dogs
    search_distinct_names.sql -> consulta para retornar os nomes distintos
    dos pets

---
+++ sql_pets/insert_cats.sql
@@ -0,0 +1,18 @@
+INSERT INTO cats (id, name)
+VALUES (1,'quizila');
+INSERT INTO cats (id, name)
+VALUES (2,'branca');
+INSERT INTO cats (id, name)
+VALUES (3,'preta');
+INSERT INTO cats (id, name)
+VALUES (4,'rato');
+INSERT INTO cats (id, name)
+VALUES (5,'lilica');
+INSERT INTO cats (id, name)
+VALUES (6,'rex');
+INSERT INTO cats (id, name)
+VALUES (7,'leka');
+INSERT INTO cats (id, name)
+VALUES (8,'pepita');
+INSERT INTO cats (id, name)
+VALUES (9,'xana');
